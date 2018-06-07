3752197aa9468a77c68b0dbddb9e5bd848445443
user1-PC\user1 <changho93@naver.com>
2018-06-07 19:13:25 +0900

    asd

---
+++ semi/src/semi/concert/controller/ConcertInfoServlet.java
@@ -0,0 +1,29 @@
+package semi.concert.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ConcertInfo", urlPatterns = { "/ConcertInfo" })
+public class ConcertInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ConcertInfoServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int indexNo = Integer.parseInt(request.getParameter("indexNo"));
+		
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
