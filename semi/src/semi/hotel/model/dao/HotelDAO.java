7c30483c35301c948422fd1df587ecd6125b608a
nuri <gsfcws112@naver.com>
2018-06-11 18:27:09 +0900

    호텔 예약창과 정보창 - 누리

---
+++ semi/src/semi/hotel/model/dao/HotelDAO.java
@@ -0,0 +1,14 @@
+package semi.hotel.model.dao;
+
+import java.sql.Connection;
+
+import semi.hotel.model.vo.HotelInfo;
+
+public class HotelDAO {
+
+	public HotelInfo selectAllInfo(Connection conn, int indexNum) {
+		
+		return null;
+	}
+
+}
