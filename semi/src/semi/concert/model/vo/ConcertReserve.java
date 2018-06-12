8c73bb9eb624b982cbb8447935ec374b4c243c3a
user1-PC\user1 <changho93@naver.com>
2018-06-12 20:32:40 +0900

    wew

--- semi/src/semi/concert/model/vo/ConcertReserve.java
+++ semi/src/semi/concert/model/vo/ConcertReserve.java
@@ -10,8 +10,6 @@ public class ConcertReserve {
 	
 	public ConcertReserve() {}
 
-	
-	
 	public String getConcertTitle() {
 		return concertTitle;
 	}
---
+++ semi/src/semi/dobo/model/vo/DoboReserve.java
@@ -0,0 +1,95 @@
+package semi.dobo.model.vo;
+
+public class DoboReserve {
+	private int reserveNo;
+	private String doboCode;
+	private String date;
+	private String time;
+	private String reserveName;
+	private String gender;
+	private String nation;
+	private String phone;
+	private String comment;
+	
+	public DoboReserve() {}
+
+	public int getReserveNo() {
+		return reserveNo;
+	}
+
+	public void setReserveNo(int reserveNo) {
+		this.reserveNo = reserveNo;
+	}
+
+	public String getDoboCode() {
+		return doboCode;
+	}
+
+	public void setDoboCode(String doboCode) {
+		this.doboCode = doboCode;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getReserveName() {
+		return reserveName;
+	}
+
+	public void setReserveName(String reserveName) {
+		this.reserveName = reserveName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getNation() {
+		return nation;
+	}
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
+	public String toString() {
+		return "reserveNo=" + reserveNo + ", doboCode=" + doboCode + ", date=" + date + ", time=" + time
+				+ ", reserveName=" + reserveName + ", gender=" + gender + ", nation=" + nation + ", phone=" + phone
+				+ ", comment=" + comment;
+	}
+	
+}
