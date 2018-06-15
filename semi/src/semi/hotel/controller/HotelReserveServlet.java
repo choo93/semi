6bab87bce7bfb26550210b9e52ec4185d6cf14cc
nuri <gsfcws112@naver.com>
2018-06-15 18:01:15 +0900

    리스트 호텔 정보 디비 연동 완료 - 누리

--- semi/src/semi/hotel/controller/HotelReserveServlet.java
+++ semi/src/semi/hotel/controller/HotelReserveServlet.java
@@ -1,11 +1,16 @@
 package semi.hotel.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import semi.hotel.model.service.HotelService;
+import semi.hotel.model.vo.HotelReserve;
 
 /**
  * Servlet implementation class HotelReserveServlet
@@ -26,8 +31,11 @@ public class HotelReserveServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+			
+		
 	}
 
 	/**
