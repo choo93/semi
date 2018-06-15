49ff94aacffc3ad1ce6c682326f69fc84de4ddfe
user1-PC\user1 <changho93@naver.com>
2018-06-15 21:50:47 +0900

    ss

--- semi/WebContent/js/concert/seatSelect.js
+++ semi/WebContent/js/concert/seatSelect.js
@@ -1,15 +1,22 @@
 
 
 var sel = false;
+var sel2 ="";
 function select(me){
 	if(!sel){
-		sel = true;
-		me.style.backgroundColor = "blue";
-		document.getElementById('seatNo').value = me.id;
-	}else{
-		sel=false;
-		me.style.backgroundColor = "white";
-		document.getElementById('seatNo').value = null;
+		if(sel2==""){
+			sel = true;
+			me.style.backgroundColor = "blue";
+			document.getElementById('seatNo').value = me.id;
+			sel2 = me.id;
+		}
+	}else{		// 선택이 0 일 때
+		if(me.id == sel2){
+			sel=false;
+			me.style.backgroundColor = "white";
+			document.getElementById('seatNo').value = null;
+			sel2 = "";
+		}
 	}
 }
 function selBool(){
